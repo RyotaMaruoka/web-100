9a576678ae2ae29e0f0267a2733d398809b382e5
Yuma Satake <satous.y@icloud.com>
2023-11-16 08:43:27 +0900

    feat: loginボタン実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+'use client';
+
+import React, { FC } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Team from './components/Team';
 import Contact from './components/Contact';
+import { useRouter } from 'next/navigation';
+import { Button } from '@material-tailwind/react';
 
-const Home = () => {
+const Home: FC = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Header />
-      Hello
+      <Button
+        onClick={() => {
+          router.push('/auth');
+        }}
+      >
+        Login
+      </Button>
       <section id="about">
         <About />
       </section>
