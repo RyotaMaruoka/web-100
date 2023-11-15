7356e0fc6056f0cdd09283fe17eaf4c7407abe0e
Yuma Satake <satous.y@icloud.com>
2023-11-16 07:00:10 +0900

    feat: エラーページ類追加

---
+++ app/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { FC } from 'react';
+
+const Error: FC = () => {
+  return (
+    <div>
+      <div className="text-center text-5xl font-bold mb-3">500</div>
+      <div className="text-center text-xl font-bold">Server Error</div>
+    </div>
+  );
+};
+
+export default Error;
