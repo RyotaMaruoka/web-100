1b3b42e94dd39855516a9c30f3e81801cf8e2d7e
Yuma Satake <satous.y@icloud.com>
2023-11-16 04:39:57 +0900

    feat: prettierrc追加

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px' }}>
+    <header
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 20px',
+      }}
+    >
       <Link href="/">
-          <img src="/images/logo.png" alt="Logo" style={{ height: '50px' }} />
-          <p>こんにちは</p>
+        <img src="/images/logo.png" alt="Logo" style={{ height: '50px' }} />
+        <p>こんにちは</p>
       </Link>
     </header>
   );
